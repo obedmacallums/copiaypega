19bbc6e4cc7def3c91759e2ff4578e7ca7f49079
obedmacallums <obedmacallums@gmail.com>
2022-08-10 20:53:50 -0400

    2793006

---
+++ _nuxt/static/1660179199/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcopiaypega\u002F_nuxt\u002Fstatic\u002F1660179199",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcopiaypega\u002F",assetsPath:"\u002Fcopiaypega\u002F_nuxt\u002F",cdnURL:a}}}}(null));
